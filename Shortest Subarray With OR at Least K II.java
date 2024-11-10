c45f7527cffbe074952de419d3253bdd766fcc48
Koushik Rai <123098956+koushikrai@users.noreply.github.com>
2024-11-10 19:28:16 +0530

    Update Shortest Subarray With OR at Least K II.java
    You are given an array nums of non-negative integers and an integer k.
    
    An array is called special if the bitwise OR of all of its elements is at least k.
    
    Return the length of the shortest special non-empty 
    subarray
     of nums, or return -1 if no special subarray exists.

--- Shortest Subarray With OR at Least K II.java
+++ Shortest Subarray With OR at Least K II.java
@@ -1,29 +1,25 @@
 class Solution {
     public int minimumSubarrayLength(int[] nums, int k) {
-    int[] count = new int[32];
-    int start = 0, end = 0, min = Integer.MAX_VALUE;
-    while(end<nums.length)
-    {
-      updateBits(count, nums[end], 1);
-      while(start<=end && getVal(count)>=k)
-      {
-        min = Math.min(min, end-start+1);
-        updateBits(count, nums[start], -1);
-        start++;
-      } end++; 
-    } 
-    return min==Integer.MAX_VALUE ? -1 : min;   
-    }
-    public void updateBits(int count[], int num, int val)
-    { for(int i=0; i<32; i++){
-        if(((num>>i)&1)==1)
-        count[i] += val;
-      }  
-    }
-    public int getVal(int[] count){
-      int ans = 0; 
-       for(int i=0; i<32; i++){
-        if(count[i]>0) ans = ans | (1<<i);
-       } return ans;
+        //atleast 1 -> xor 1
+        // shortest special non-empty subarray
+        // -1 if no special subarray exists
+       int w = 0, min = Integer.MAX_VALUE;
+        int[] bits = new int[32]; // bits' counts
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            if (nums[r] >= k) return 1;
+            w |= nums[r];
+            for (int i = 0; i < bits.length; i++) {
+                bits[i] += (nums[r] >> i) & 1;
+            }
+            while (w >= k) {
+                min = Math.min(min, r - l + 1);
+                for (int i = 0; i < bits.length; i++) {
+                    bits[i] -= (nums[l] >> i) & 1;
+                    if (bits[i] == 0) w &= ~(1 << i);
+                }
+                l++;
+            } 
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
 }
